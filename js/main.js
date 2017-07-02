77fbfd3f5e6770ef37828ba5f2dc9dbeafb7dd87
Piotr Lewinski <piotrlew5@wp.pl>
2017-07-02 14:33:57 +0200

    mix everything

--- js/main.js
+++ js/main.js
@@ -2,12 +2,32 @@
 
 $(function () {
     
-    $(window).scroll(function () {
+/* $(window).scroll(function () {
         if ($(window).scrollTop() + $(window).height() > $(window).height() ) {
             //color nawigacji
             $('#main-nav').css('background-color', 'aqua');
         } else {
             $('#main-nav').css('background-color', 'transparent');
         }
+    });*/
+    
+    
+    $(window).scroll(function () {
+       
+        if ($(window).scrollTop() >= 50) {
+            //$('#main-nav').css('background-color', 'aqua');
+            $('#main-nav').addClass('scroll');
+            $('#main-nav').css({'transition': 'all 0.3s'});
+        } else {
+            //$('#main-nav').css('background-color', 'transparent');
+            $('#main-nav').removeClass('scroll');
+        }
+        
     });
+
+     if ($(window).scrollTop() >= 50) {
+            $('#main-nav').addClass('scroll');
+         
+        } 
+    
 });
